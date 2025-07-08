511035d599a30408c31053b2bf375dcd0a08f3ee
takeshi-kiyota <takeshi-kiyota@bm-sms.co.jp>
2025-07-08 23:39:53 +0900

    feat: employee の一件取得を追加

--- serviceA/request.md
+++ serviceA/request.md
@@ -5,3 +5,9 @@
 ```bash
 curl -s http://localhost:8080/employees | jq .
 ```
+
+## 従業員一件取得
+
+```bash
+curl -s http://localhost:8080/employees/1 | jq .
+```
--- serviceA/src/main/kotlin/observability/com/example/serviceA/controller/EmployeeController.kt
+++ serviceA/src/main/kotlin/observability/com/example/serviceA/controller/EmployeeController.kt
@@ -3,6 +3,7 @@ package observability.com.example.serviceA.controller
 import observability.com.example.serviceA.entity.Employee
 import observability.com.example.serviceA.repository.EmployeeRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,5 +16,10 @@ class EmployeeController(
     @GetMapping
     fun getAllEmployees(): List<Employee> {
         return employeeRepository.findAll()
+    }
+    
+    @GetMapping("/{id}")
+    fun getEmployeeById(@PathVariable id: Long): Employee? {
+        return employeeRepository.findById(id).orElse(null)
     }
 }

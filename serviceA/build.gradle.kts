3459a2208b9ea2d2a7e956abd01d105cb9649042
takeshi-kiyota <takeshi-kiyota@bm-sms.co.jp>
2025-07-14 09:21:54 +0900

    serviceA の metrics は grafana でみれたっぽい(まったくわからん)

--- serviceA/build.gradle.kts
+++ serviceA/build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("io.micrometer:micrometer-registry-prometheus")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("com.apollographql.apollo3:apollo-runtime:3.8.4")
